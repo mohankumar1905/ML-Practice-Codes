91aecbef9f1cca913b97c0121a657e97a38027cd
Mohankumar Balasubramaniyam <47718973+mohankumar1905@users.noreply.github.com>
2020-03-19 07:29:34 +0530

    Create selenium-colab.py

---
+++ selenium-colab.py
@@ -0,0 +1,13 @@
+!pip install selenium
+!sudo apt-get update # to update ubuntu to correctly run apt install
+!sudo apt install chromium-chromedriver --yes
+!cp /usr/lib/chromium-browser/chromedriver /usr/bin
+import sys
+sys.path.insert(0,'/usr/lib/chromium-browser/chromedriver')
+from selenium import webdriver
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+wd = webdriver.Chrome('chromedriver',chrome_options=chrome_options)
+wd.get("https://www.webite-url.com")
